9f5b20e9156d0600f75bd972bdbadf4278615f47
SumoPikmin <pikminzwiebel@gmail.com>
2025-05-06 12:11:58 +0800

    more training

--- student_agent.py
+++ student_agent.py
@@ -16,7 +16,7 @@ def _preprocess_frame(frame: np.ndarray) -> np.ndarray:
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     frame = cv2.resize(frame, (84, 84), interpolation=cv2.INTER_AREA)
     return frame
-
+ 
 # ------------------------------------------------------------
 # Updated DQN (Matches Training Architecture)
 # ------------------------------------------------------------
